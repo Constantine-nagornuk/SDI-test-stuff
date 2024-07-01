0b5b9d53dcadc1f7f9418b30eadb4009eb4812c0
Constantine-nagornuk <conplay55@gmail.com>
2024-07-01 12:49:44 -0400

    all is working
    read comments and im still a mad man

---
+++ test.py
@@ -0,0 +1,43 @@
+#get sqlite extension
+import sqlite3
+con = sqlite3.connect("Library.db")
+cur = con.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS Library(Name,ReturnDays,Count )")
+
+
+
+
+
+cur.execute("""
+    INSERT INTO Library VALUES
+        ('Book1', '20',1 ),
+        ('Book2', '14' ,4),
+        ('Book3', '11',3 ),
+        ('Book4', '4',2 )    
+""")
+
+UserBooks = []
+getbook = input("Enter the name of the book you wish to take out? ")
+UserBooks.append(getbook)
+print(UserBooks)
+
+res = cur.execute("""SELECT Count FROM Library WHERE Name=? """,(getbook,))
+
+print(res.fetchall())
+
+cur.execute("""update Library set Count=? where Name=?""", (3, getbook)) # can now update the table base by changing values
+con.commit()
+#handle book loans, returns, and inventory.
+# loans - take the book out of stock and and ssign the user a certain amount of days before it must be returend
+# return- when you return the value goes back to orginal state
+# invotry- name of book, , (is book taken out), 
+# make a user invotry system
+
+
+
+Loan = None # how many books loaned to you
+Books = None # Name of books loaned to you/ How many you have
+
+
+
